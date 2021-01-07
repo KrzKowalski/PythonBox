1b175e168caed311d43eed99477d4a2a7e91d8b5
KrzKowalski <kkowalski1@protonmail.com>
2021-01-07 19:59:54 +0100

    An OOP project of a DiceGame - shall be developed along with learning of OOP.

---
+++ DiceGame/main.py
@@ -0,0 +1,6 @@
+#TODO Wybór graczy - od 1 do 4?
+#TODO Tablica wyników - pretty table?
+#TODO Dodanie ASCII art do kości
+#TODO Gracz rzuca 5 losowych kości
+#TODO Gracz może zostawić sobie dowolną ilość kości w "ręce"
+#TODO W grze jest 13 (chyba) tur
